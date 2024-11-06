e63e8775ec6b60f52328ab82b5132237ffd2195c
Marcos Chocce <fernando.chocce@tecsup.edu.pe>
2024-11-06 13:58:19 -0500

    Test web controller create table Owner

---
+++ src/test/java/com/tecsup/petclinic/webs/OwnerControllerTest.java
@@ -0,0 +1,66 @@
+package com.tecsup.petclinic.webs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.tecsup.petclinic.domain.OwnerTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Slf4j
+public class OwnerControllerTest {
+
+    private static final ObjectMapper om = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //Chocce
+    @Test
+    public void testCreateOwner() throws Exception {
+
+        String FIRST_NAME = "Leo";
+        String LAST_NAME = "Basil";
+        String ADDRESS = "Calle 1";
+        String CITY = "Santo Domingo";
+        String TELEPHONE = "123456789";
+
+        OwnerTO newOwnerTO = new OwnerTO();
+        newOwnerTO.setFirstName(FIRST_NAME);
+        newOwnerTO.setLastName(LAST_NAME);
+        newOwnerTO.setAddress(ADDRESS);
+        newOwnerTO.setCity(CITY);
+        newOwnerTO.setTelephone(TELEPHONE);
+
+        mockMvc.perform(post("/owners")
+                        .content(om.writeValueAsString(newOwnerTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(FIRST_NAME)))
+                .andExpect(jsonPath("$.lastName", is(LAST_NAME)))
+                .andExpect(jsonPath("$.address", is(ADDRESS)))
+                .andExpect(jsonPath("$.city", is(CITY)))
+                .andExpect(jsonPath("$.telephone", is(TELEPHONE)));
+
+    }
+
+}
+/**
+ *
+ * @throws Exception
+ */
